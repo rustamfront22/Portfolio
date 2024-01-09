e2195f242ab777f7e8cdc8f1de9e8f9bdc35fd91
rustam <rustam.front@gmail.com>
2024-01-09 20:11:30 +0500

    изменение в вёрстке

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,9 +1,16 @@
 <template>
     <div class="header">
         <div class="header-content container">
-            <div class="logo">
-                <img src="@/assets/img/logo.svg" alt="">
-            </div>
+            <div class="pyramid-loader">
+  <div class="wrapper">
+    <span class="side side1"></span>
+    <span class="side side2"></span>
+    <span class="side side3"></span>
+    <span class="side side4"></span>
+    <span class="shadow"></span>
+  </div>
+</div>
+
         </div>
     </div>
 </template>
--- src/components/Project/Project.vue
+++ src/components/Project/Project.vue
@@ -6,17 +6,28 @@
       </div>
       <h2>My Projects Highlight</h2>
       <a href="https://github.com/rustamfront22"
-        ><button class="btn">
-          Explore More
-          <Icon
-            icon="mingcute:arrow-up-fill"
-            color="white"
-            :rotate="1"
-          /></button
-      ></a>
+        ><button>
+          <svg
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="css-i6dzq1"
+          >
+            <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"></polygon>
+          </svg>
+          Explore me
+        </button>
+      </a>
       <div class="project-content-box">
         <div class="project-content-box__info">
-          <a href="https://market-olive.vercel.app/"><img src="@/assets/img/project-1.png" alt="" /></a>
+          <a href="https://market-olive.vercel.app/"
+            ><img src="@/assets/img/project-1.png" alt=""
+          /></a>
           <h3>
             Market
             <div></div>
@@ -27,7 +38,9 @@
           </p>
         </div>
         <div class="project-content-box__info">
-          <a href="https://githubfinder-kohl.vercel.app/"><img src="@/assets/img/project-2.png" alt="" /></a>
+          <a href="https://githubfinder-kohl.vercel.app/"
+            ><img src="@/assets/img/project-2.png" alt=""
+          /></a>
           <h3>
             GitHubFinder
             <div></div>
@@ -39,7 +52,9 @@
           </p>
         </div>
         <div class="project-content-box__info">
-          <a href="https://todolist-olive-seven.vercel.app/"><img src="@/assets/img/project-3.png" alt="" /></a>
+          <a href="https://todolist-olive-seven.vercel.app/"
+            ><img src="@/assets/img/project-3.png" alt=""
+          /></a>
           <h3>
             TodoList
             <div></div>
@@ -51,7 +66,9 @@
           </p>
         </div>
         <div class="project-content-box__info">
-          <a href="https://wheather-ruddy.vercel.app/"><img src="@/assets/img/project-4.png" alt="" /></a>
+          <a href="https://wheather-ruddy.vercel.app/"
+            ><img src="@/assets/img/project-4.png" alt=""
+          /></a>
           <h3>
             Wheather
 
