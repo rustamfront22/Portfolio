0953cbcc3871c9cd86808903403cff61e72e7ba4
rustam <rustam.front@gmail.com>
2024-01-29 20:19:49 +0500

    исправление багов

--- src/components/Nav/Nav.vue
+++ src/components/Nav/Nav.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="header">
-    
     <div class="header-content container">
       <div class="pyramid-loader">
         <div class="wrapper">
@@ -11,11 +10,33 @@
           <span class="shadow"></span>
         </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script setup>
 import { Icon } from "@iconify/vue";
+import { createApp, ref, onMounted } from "vue";
+
+
 </script>
+
+<style>
+#game-board {
+  width: 600px;
+  height: 400px;
+  border: 1px solid black;
+}
+
+.snake {
+  width: 20px;
+  height: 20px;
+  background-color: green;
+}
+
+.food {
+  width: 20px;
+  height: 20px;
+  background-color: red;
+}
+</style>
